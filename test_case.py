4faf5e06ec89e3967ecffb75e43e611387cd3db4
igoreduardobraga <igoredubraga@gmail.com>
2025-09-26 14:15:26 -0300

    4. Testando TestCase

--- test_case.py
+++ test_case.py
@@ -20,33 +20,6 @@ class TestCase:
 
     def tear_down(self):
         pass
-    
-
-class TestResult:
-
-    RUN_MSG = 'run'
-    FAILURE_MSG = 'failed'
-    ERROR_MSG = 'error'
-
-    def __init__(self, suite_name=None):
-        self.run_count = 0
-        self.failures = []
-        self.errors = []
-
-    def test_started(self):
-        self.run_count += 1
-
-    def add_failure(self, test):
-        self.failures.append(test)
-
-    def add_error(self, test):
-        self.errors.append(test)
-
-    def summary(self):
-        return f'{self.run_count} {self.RUN_MSG}, ' \
-            f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
-            f'{str(len(self.errors))} {self.ERROR_MSG}'
-
 
 class MyTest(TestCase):
 
@@ -64,17 +37,3 @@ class MyTest(TestCase):
 
     def test_c(self):
         print('test_c')
-
-
-result = TestResult()
-
-test = MyTest('test_a')
-test.run(result)
-
-test = MyTest('test_b')
-test.run(result)
-
-test = MyTest('test_c')
-test.run(result)
-
-print(result.summary())
